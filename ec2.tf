27a291d6d3bb0fb06d7eb3a528388eec81cd063a
Akiki <charbelakiki1111@gmail.com>
2023-01-10 19:44:18 +0000

    removed aws cred

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "worldbank" {
   ami           = "ami-06878d265978313ca"
+  count = 3
   instance_type = "t2.micro"
   key_name = "deployer-key" # aws_key_pair.worldbank.key_name
   security_groups = [aws_security_group.worldbank.name]
--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
 output "worldbank_public_IP" {
-  value = aws_instance.worldbank.public_ip
+  # value = aws_instance.worldbank.public_ip
+  value = aws_instance.worldbank*.public_ip
 }
