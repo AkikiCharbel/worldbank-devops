35bc3995d6961e3c3cd5b9766ca0be5a429134fb
Akiki <charbelakiki1111@gmail.com>
2023-01-10 21:10:32 +0000

    included loops

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 variable "ec2_count" {
-  default = 1
+  default = 5
 }
 
 resource "aws_instance" "worldbank" {
@@ -8,4 +8,24 @@ resource "aws_instance" "worldbank" {
   instance_type   = "t2.micro"
   key_name        = "deployer-key" # aws_key_pair.worldbank.key_name
   security_groups = [aws_security_group.worldbank.name]
+  tags = {
+    name = "TerraformMachine-${count.index}"
+  }
+}
+
+
+variable "tags_list" {
+  type    = list(string)
+  default = ["devops", "hello1", "cm", "iac", "cd"]
+}
+
+resource "aws_instance" "resource-from-list" {
+  ami             = "ami-06878d265978313ca"
+  instance_type   = "t2.micro"
+  key_name        = "deployer-key" # aws_key_pair.worldbank.key_name
+  security_groups = [aws_security_group.worldbank.name]
+  tags = {
+    Name = each.value
+  }
+  for_each = toset(var.tags_list)
 }
